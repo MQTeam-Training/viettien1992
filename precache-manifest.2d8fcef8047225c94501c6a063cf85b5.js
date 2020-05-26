7215e742298ef355bcf87884335247df7a1b1592
viettien1992 <viettien321992@gmail.com>
2020-05-26 11:59:46 +0700

    1

---
+++ precache-manifest.2d8fcef8047225c94501c6a063cf85b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfbe2781a45eeaaa9784b101f0840ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "893305447995ef07df4e",
+    "url": "/static/css/main.767fb10e.chunk.css"
+  },
+  {
+    "revision": "e9777748462064b50829",
+    "url": "/static/js/2.286dd105.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.286dd105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893305447995ef07df4e",
+    "url": "/static/js/main.9a4fe845.chunk.js"
+  },
+  {
+    "revision": "a0ae03afd03be7158f22",
+    "url": "/static/js/runtime-main.99970e07.js"
+  }
+]);
